453fd95c2a7f8da9260e92fb038dce27bab900b7
MiuraMao <m_miura06@outlook.com>
2021-03-10 08:16:50 +0900

    cipher

--- 2-4.c
+++ 2-4.c
@@ -8,7 +8,7 @@ int main(void){
     scanf("%c", &plain);
     printf("暗号キー？");
     scanf("%d", &key);
-    cipher = (plain - 97 + key) % 26 + 97;  //暗号化(aの文字コードは97)
+    cipher = (plain - 97 + key) % 26 + 97;  //暗号化(aの文字コードは97) %は+や-より優先
     printf("暗号文は「%c」\n", cipher);
     return 0;
 }
