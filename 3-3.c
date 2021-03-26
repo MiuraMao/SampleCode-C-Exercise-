e73676d33f51811977d30fc3aad636f9b49b73fe
MiuraMao <m_miura06@outlook.com>
2021-03-26 21:51:30 +0900

    Switch

--- 3-3.c
+++ 3-3.c
@@ -11,7 +11,7 @@ int main(void){
     printf("演算を選んで下さい 1:加算, 2:減算, 3:乗算, 4:除算 ？ ");
     scanf("%d", &oper);
     switch (oper){
-        case 1:
+        case 1:         //ラベル定数は整数に限られる
             answer = x + y;
             break;
         case 2:
