99f77eb5a58b078b6b601b0017dfd822c3395c4a
MiuraMao <m_miura06@outlook.com>
2021-04-14 21:36:29 +0900

    Times Tables

---
+++ 3-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int i, j;
+
+    printf("     | 1| 2| 3| 4| 5| 6| 7| 8| 9|\n");
+    printf("-----+--+--+--+--+--+--+--+--+--+\n");
+    for (i = 1; i <= 9; i++){
+        printf("%dの段|", i);
+        /*iの段を表示する*/
+        for (j = 1; j <= 9; j++)
+            printf("%2d|", i*j);
+        printf("\n");
+    }
+    return 0;
+}
