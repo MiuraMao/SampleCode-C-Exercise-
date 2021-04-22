9c75d57100541eb9baa94beedafaa9386047b792
MiuraMao <m_miura06@outlook.com>
2021-04-22 20:47:11 +0900

    Returned Value

---
+++ 4-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+double round_to(double, int);       //関数プロトタイプ
+
+/*xを四捨五入して小数点以下n桁までにする*/
+double round_to(double x, int n){
+    double mul = 1, result;
+    
+    for (int i = 0; i < n; i++)     //10のn乗を求める
+        mul *= 10;
+    result = (int)(x * mul + 0.5) / mul;    //四捨五入
+    return result;                  //resultの値を戻り値として戻る
+}
+
+int main(void){
+    double val, rounded;
+    int n;
+
+    printf("数値？ ");
+    scanf("%lf", &val);
+    printf("小数点以下の桁数？ ");
+    scanf("%d", &n);
+    rounded = round_to(val, n);     //valを小数点以下n桁までにする
+    printf("%f\n", rounded);
+    return 0;
+}
