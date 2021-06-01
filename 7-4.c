091e83998de371a727ae08c3c2bf29b92a671d08
MiuraMao <m_miura06@outlook.com>
2021-06-01 23:18:52 +0900

    toupper

--- 7-4.c
+++ 7-4.c
@@ -3,15 +3,15 @@
 //行頭の文字を大文字にする
 
 int main(void){
-    int c, pc;              //pc: １つ前の文字（previous character）
+    int c, pc;                          //pc: １つ前の文字（previous character）
 
-    pc = '\n';              //最初の文字を行頭扱いにするため
+    pc = '\n';                          //最初の文字を行頭扱いにするため
     while ((c = getchar()) != EOF){
-            if (pc == '\n')     //いま行頭なら
+            if (pc == '\n')             //いま行頭なら
                 putchar(toupper(c));    //小文字なら大文字に、それ以外はそのまま
-            else                //そうでなければ
-                putchar(c);     //cをそのまま出力
-            pc = c;             //いま出力した文字を覚えておく
+            else                        //そうでなければ
+                putchar(c);             //cをそのまま出力
+            pc = c;                     //いま出力した文字を覚えておく
     }
     return 0;
 }
